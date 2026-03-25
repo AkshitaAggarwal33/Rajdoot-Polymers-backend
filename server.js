bd257211cbe6a109b2339a06cb919fb0235d3f5a
Akshita Aggarwal <akshitaaggarwal2004@gmail.com>
2026-03-25 23:00:06 +0530

    update server js

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ mongoose
 app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀")
+})
 
 app.listen(PORT, () => {
   console.log("Server running on port 5000");
