9778429eb159b56a8fa1d8b468393f38334af64f
Akshita Aggarwal <akshitaaggarwal2004@gmail.com>
2026-03-25 22:49:50 +0530

    port issue update

--- server.js
+++ server.js
@@ -20,6 +20,8 @@ mongoose
 // Routes
 app.use("/api/users", userRoutes);
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
   console.log("Server running on port 5000");
 });
